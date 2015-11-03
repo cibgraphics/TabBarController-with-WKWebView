7e0a446ecb464f0d2b2f46612ba2c07902501faa
Kaden Wilkinson <wilkinson.kaden@gmail.com>
2015-11-03 13:16:10 -0700

    Initial Commit

---
+++ LDSG/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LDSG
+//
+//  Created by Kaden Wilkinson on 11/3/15.
+//  Copyright © 2015 Trendly Creations. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
